5f23d01f2938767e06d29eb1704d1e8a6da2ff8e
Tom E Flaherty <67797311+teflaherty67@users.noreply.github.com>
2025-09-01 19:36:21 -0500

    in progress

---
+++ LD_OnStartUp/Forms/frmModelType.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LD_OnStartUp.Forms
+{
+    /// <summary>
+    /// Interaction logic for frmModelType.xaml
+    /// </summary>
+    public partial class frmModelType : Window
+    {
+        public frmModelType()
+        {
+            InitializeComponent();
+        }
+
+        public string GetGroupProjectType()
+        {
+            if (rbnNew.IsChecked == true)
+                return rbnNew.Content.ToString();
+            else if (rbnExisting.IsChecked == true)
+                return rbnExisting.Content.ToString();
+            else
+                return null;
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+
+        private void btnHelp_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ LD_OnStartUp/Forms/frmProjectSetup.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LD_OnStartUp.Forms
+{
+    /// <summary>
+    /// Interaction logic for frmProjectSetup.xaml
+    /// </summary>
+    public partial class frmProjectSetup : Window
+    {
+        public frmProjectSetup()
+        {
+            InitializeComponent();
+        }
+    }
+}
